4972c56da02054a233a89f49e92028b1dd1c8918
phumulani thusi <thusiphumulani1@gmail.com>
2024-10-29 09:51:14 +0200

    fixed the conditional

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 def calculate_discount(price, discount_percent):
-    if discount_percent<=(20/100):
+    if discount_percent>=(20/100):
         return price - price * discount_percent
     return price
 
@@ -7,3 +7,5 @@ def run_discount():
     price = float(input('Enter the original price of an item: '))
     discount_percent = float(input('Enter the discount percentage: '))
     return calculate_discount(price, discount_percent)
+
+print(run_discount())
