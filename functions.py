44329aa487aab8eab4ae349052500ca44a898056
phumulani thusi <thusiphumulani1@gmail.com>
2024-10-29 09:49:06 +0200

    fixed my mistakes

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 def calculate_discount(price, discount_percent):
-    if discount_percent>=(20/100):
+    if discount_percent<=(20/100):
         return price - price * discount_percent
     return price
 
-def calculate_discount():
-    price = input('Enter the original price of an item: ')
-    discount_percent = input('Enter the discount percentage: ')
+def run_discount():
+    price = float(input('Enter the original price of an item: '))
+    discount_percent = float(input('Enter the discount percentage: '))
     return calculate_discount(price, discount_percent)
